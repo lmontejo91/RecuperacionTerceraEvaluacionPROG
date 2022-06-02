f9a09b33ce9681676d36ac08fbe4c8009e249cce
lmontejo91 <l.montejov.91@gmail.com>
2022-06-02 20:11:34 +0200

    tercer commit

--- src/main/java/com/mycompany/recuterceraevaluacion_luciamontejo/Bomberos.java
+++ src/main/java/com/mycompany/recuterceraevaluacion_luciamontejo/Bomberos.java
@@ -7,9 +7,13 @@ package com.mycompany.recuterceraevaluacion_luciamontejo;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  *
@@ -18,6 +22,7 @@ import java.util.Scanner;
 public class Bomberos {
     static ArrayList<String[]> datos_Intervenciones=new ArrayList<>();
     static String datos_porLinea[]=new String[11];
+    static Map<String, Integer> map_Intervenciones_porDistrito = new HashMap<String, Integer>();
     
     //MÉTODO que va a leer los archivos y almacenar los datos necesarios.
     public static void leerArchivos(File ruta){
@@ -48,10 +53,12 @@ public class Bomberos {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
+        
     }
     
-    public static void IntervencionConMasYMenosSalidas(){
-        int contador=0;
+    public static String IntervencionConMasYMenosSalidas(){
+        int contador=0, intervencion_MasSalidas=0, intervencion_MenosSalidas=0;
+        String nombre_intervenciones_MasSalidas=null, nombre_intervenciones_MenosSalidas=null;
         Map<String, Integer> map_salidasTotales_porIntervenion = new HashMap<String, Integer>()
         {{
              put("FUEGOS",0);
@@ -82,15 +89,66 @@ public class Bomberos {
         
         System.out.println(map_salidasTotales_porIntervenion);
         
+        intervencion_MasSalidas=Collections.max(map_salidasTotales_porIntervenion.values());
+        intervencion_MenosSalidas=Collections.min(map_salidasTotales_porIntervenion.values());
+        map_salidasTotales_porIntervenion.containsValue(12040);
+        
+        for (String key : getKeys(map_salidasTotales_porIntervenion, intervencion_MasSalidas)) {
+            nombre_intervenciones_MasSalidas=nombre_intervenciones_MasSalidas.concat(key+", ");
+        }
+        
+        for (String key : getKeys(map_salidasTotales_porIntervenion, intervencion_MenosSalidas)) {
+            nombre_intervenciones_MenosSalidas=nombre_intervenciones_MenosSalidas.concat(key+", ");
+        }
+        
+        return "La intervención o intervenciones con MÁS salidas son las de "
+                +nombre_intervenciones_MasSalidas+" con un número total de "+intervencion_MasSalidas
+                +"\nLa intervención o intervenciones con MENOS salidas son las de "
+                +nombre_intervenciones_MenosSalidas+" con un número total de "+intervencion_MenosSalidas;        
     }
     
+    public static void numeroIntervenciones_porDistrito(){   
+        for(String[] elemento : datos_Intervenciones){
+            if(!map_Intervenciones_porDistrito.containsKey(elemento[2])){
+                map_Intervenciones_porDistrito.put(elemento[2], Integer.parseInt(elemento[10]));
+            }else{
+                map_Intervenciones_porDistrito.put(elemento[2], map_Intervenciones_porDistrito.get(elemento[2])+Integer.parseInt(elemento[10]));
+            }
+        }
+        
+        System.out.println(map_Intervenciones_porDistrito);
+        
+    }
+    
+    public static Set<String> getKeys(Map<String, Integer> map, Integer value) {
+
+      Set<String> result = new HashSet<>();
+      if (map.containsValue(value)) {
+          for (Map.Entry<String, Integer> entry : map.entrySet()) {
+              if (Objects.equals(entry.getValue(), value)) {
+                  result.add(entry.getKey());
+              }
+          }
+      }
+      return result;
+
+  }
+    
     public static void main(String[] args) {
+        Scanner teclado=new Scanner(System.in);
         File directorio_Bomberos = new File("Bomberos");
+        String distrito;
     
         //System.out.println(directorio_Bomberos.isDirectory());
         //System.out.println(directorio_Bomberos.exists());
         leerArchivos(directorio_Bomberos);
+        IntervencionConMasYMenosSalidas();
+        numeroIntervenciones_porDistrito();
         
+        System.out.println("-----------------------------------------------------------------");
+        System.out.println("¿De qué distrito quiere quiere mostrar información?");
+        distrito=teclado.nextLine();
+        System.out.println(distrito+" tuvo un número total de intervenciones de "+map_Intervenciones_porDistrito.get(distrito.toUpperCase()));
     }
     
 }
